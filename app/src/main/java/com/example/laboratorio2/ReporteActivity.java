8409ac78f270a9badff97c16ef4e3cc65b47b9d6
AgustinVizcarra <a20181454@pucp.edu.pe>
2022-09-16 09:18:51 -0500

    AvanceReporte

--- app/src/main/java/com/example/laboratorio2/ReporteActivity.java
+++ app/src/main/java/com/example/laboratorio2/ReporteActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReporteActivity extends AppCompatActivity {
 
@@ -15,9 +16,24 @@ public class ReporteActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Lista lista = (Lista) intent.getSerializableExtra("lista");
         if(lista.getListaEquipos().size()==0){
-
+            //No hay equipos
+            TextView textView = findViewById(R.id.reporteTextView);
+            textView.setText("No se han encontrado equipos añadidos :(");
         }else{
 
         }
+    }
+    public String buscarComputadoras(Lista lista){
+        String result = "Computadoras : \n";
+
+        return result;
+    }
+    public String buscarMonitores(Lista lista){
+        String result = "Teclado : ";
+        return result;
+    }
+    public String buscarTeclado(Lista lista){
+        String result = "Monitor : ";
+        return result;
     }
 }
