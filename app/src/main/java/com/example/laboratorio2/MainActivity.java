0a73f0d77f1e9391c834fde73f42af5559ef738e
AgustinVizcarra <a20181454@pucp.edu.pe>
2022-09-16 08:29:08 -0500

    standByRedirection

--- app/src/main/java/com/example/laboratorio2/MainActivity.java
+++ app/src/main/java/com/example/laboratorio2/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.laboratorio2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Se agrega una Lista de objetos
+    List<Object> listaEquipos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Redireccion al registro de Computadora
+    public void registroComputadora(View view){
+        Intent intent = new Intent(this,);
+        startActivity(intent);
     }
 }
