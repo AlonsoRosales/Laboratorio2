a175b0e8aa80dea01f6685470927fa1eb3559d41
Manuel <a20190133@pucp.edu.pe>
2022-09-16 08:37:33 -0500

    Primer commit monitor

--- app/src/main/java/com/example/laboratorio2/MainActivity.java
+++ app/src/main/java/com/example/laboratorio2/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.laboratorio2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void listaMonitores(View view) {
+        Intent intent = new Intent(this, MainActivity_MonitorListar.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/laboratorio2/Monitor.java
@@ -0,0 +1,61 @@
+package com.example.laboratorio2;
+
+import java.io.Serializable;
+
+public class Monitor implements Serializable {
+
+    private String activo;
+    private String pc;
+    private String marca;
+    private String pulgadas;
+    private String anio;
+    private String modelo;
+
+    public String getActivo() {
+        return activo;
+    }
+
+    public void setActivo(String activo) {
+        this.activo = activo;
+    }
+
+    public String getPc() {
+        return pc;
+    }
+
+    public void setPc(String pc) {
+        this.pc = pc;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPulgadas() {
+        return pulgadas;
+    }
+
+    public void setPulgadas(String pulgadas) {
+        this.pulgadas = pulgadas;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public void setAnio(String anio) {
+        this.anio = anio;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+}
