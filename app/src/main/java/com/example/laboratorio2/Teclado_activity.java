518e5c3524a1f8853daffd513f8abe3d74eb8a88
Ronny Pastor <a20185534@pucp.edu.pe>
2022-09-18 15:42:53 -0500

    Teclado avance

--- app/src/main/java/com/example/laboratorio2/Teclado_activity.java
+++ app/src/main/java/com/example/laboratorio2/Teclado_activity.java
@@ -1,10 +1,12 @@
 package com.example.laboratorio2;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ListView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -15,18 +17,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Teclado_activity extends AppCompatActivity {
-
+    public Lista maquinas = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teclado);
-        ScrollView scrollView = (ScrollView) findViewById(R.id.lista_teclados);
+        getSupportActionBar().setTitle("Teclado");
+        ActionBar actionBar =  getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
         Lista lista = (Lista) intent.getSerializableExtra("lista");
-        TextView vacio = new TextView(Teclado_activity.this);
-        vacio.setText("No hay teclados ingresados");
+        maquinas = lista;
+
+        ListView listView = (ListView) findViewById(R.id.listaTeclados);
+        String listadoTeclado = "";
+
+
         if (lista.listaEquipos.isEmpty()){
-            scrollView.addView(vacio);
+            listadoTeclado += "No hay teclados ingresados";
+            TextView textView = findViewById(R.id.teclado_textview_vacio);
+            textView.setText(listadoTeclado);
             //Si esta vacio por defecto se pone No hay teclados ingresadas
         }else{
             List<Teclado> tecladoList = new ArrayList<>();
@@ -35,9 +45,18 @@ public class Teclado_activity extends AppCompatActivity {
                     tecladoList.add((Teclado) obj);
                 }
             }
+            ArrayList<String> listaTextos = new ArrayList<>();
+            String texto = "";
+            for (Teclado teclado:tecladoList) {
+                texto += "Activo: " + teclado.getActivo()+ "\n" +"PC: " + teclado.getPC() + "\n" + "marca: " + teclado.getMarca() + "\n" + "Año: " + teclado.getAnho() + "\n" + "idioma: " + teclado.getIdioma() + "\n" + "Modelo: " + teclado.getModelo() + "\n";
+                listaTextos.add(texto);
+                texto = "";
+            }
+
             if (tecladoList.isEmpty()){
-                scrollView.addView(vacio);
-                //Si esta vacio por defecto se pone No hay teclados ingresadas
+                listadoTeclado += "No hay teclados ingresados";
+                TextView textView = findViewById(R.id.teclado_textview_vacio);
+                textView.setText(listadoTeclado);
             }else{
                 //Se lista los teclados
             }
